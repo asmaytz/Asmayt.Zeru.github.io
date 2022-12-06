afa277e5fee73ac063b33b5916c7d76e33663a4b
Asmayt Zeru <77589400+asmaytz@users.noreply.github.com>
2022-12-06 00:06:05 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
 <script >
-alert("Hello Welcome to my Portolio! ");
-prompt("what is your name?");
-alert("Welcome[name]!");
+function fetchData() {
+        
+        fetch(new Request('degree.json'))
+              
+                .then((res) => {
+                      
+                        console.log(res.status, res.statusText);
+                      
+                        return res.json();
+                })
+              
+                .then((degree) => {
+                        console.log(degree);
+                })
+                //handling error
+                .catch((err) => {
+                        console.log(err);
+                })
+}
 
-
+//triggering event
+document.getElementById('event-occur').addEventListener('click', fetchData);
 
 </script>
 
